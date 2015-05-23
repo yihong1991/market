97e26754dfa87918186f9a675c6f548f6f0c9f40
Peijin Sun <Stefanie@Peijins-MacBook-Pro.local>
2015-05-23 15:59:02 -0700

    add shortcut icon

--- app/views/include/head.blade.php
+++ app/views/include/head.blade.php
@@ -3,6 +3,7 @@
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 <link rel="stylesheet" type="text/css" href="http://localhost/css/reset.css">
 <link rel="stylesheet" type="text/css" href="http://localhost/css/city.css">
+<link rel="shortcut icon" type="image/x-icon" href="images/shortcut.png"> 
 <script type="text/javascript" src="http://localhost/js/jquery-1.9.1.min.js"></script>
 
 
